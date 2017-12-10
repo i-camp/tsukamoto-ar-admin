81e1e1859a994cda97a6a32cb167c4bc27904e29
nakahararuu <repika02@gmail.com>
2017-12-11 05:27:15 +0900

    お助けイベントに演出を追加

--- src/app/live-game-monitor/live-game-monitor.component.ts
+++ src/app/live-game-monitor/live-game-monitor.component.ts
@@ -1,29 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from "../game.service";
 import { ActivatedRoute } from "@angular/router";
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {trigger, state, style, transition, animate, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'ta-live-game-monitor',
   templateUrl: './live-game-monitor.component.html',
   animations: [
-    trigger('helperEvent', [
+    trigger('helperCutin', [
       state('in', style({
-        transform: 'translateX(calc(100vw - 1150px))'
+        transform: 'translateX(-200vw)'
       })),
       transition('void => in', [
-        style({transform: 'translateX(100vw)'}),
-        animate(800)
-      ]),
-      transition('in => void', [
-        animate('200ms 1000ms', style({transform: 'translateX(-100vw)'}))
-      ]),
+        animate(4000, keyframes([
+          style({transform: 'translateX(100vw)', offset: 0}),
+          style({transform: 'translateX(calc(100vw - 1150px))', offset: 0.1}),
+          style({transform: 'translateX(calc(100vw - 1250px))', offset: 0.9}),
+          style({transform: 'translateX(-200vw)', offset: 1})
+        ]))
+      ])
+    ]),
+    trigger('helperCaption', [
+      state('in', style({
+        transform: 'scale(0)'
+      })),
+      transition('void => in', [
+        animate(4000, keyframes([
+          style({transform: 'scale(0)', offset: 0}),
+          style({transform: 'scale(1)', offset: 0.1}),
+          style({transform: 'scale(1)', offset: 0.9}),
+          style({transform: 'scale(0)', offset: 1})
+        ]))
+      ])
     ])
   ]
 })
 export class LiveGameMonitorComponent implements OnInit {
 
-  private helperEffectState: 'in';
+  private helperEffectState:  'in';
+  private helpedTarget: any;
 
   constructor(private route: ActivatedRoute,
               private gameService: GameService) {
@@ -39,6 +54,7 @@ export class LiveGameMonitorComponent implements OnInit {
     this.gameService.helperEventEmitter
       .subscribe(worstTarget => {
         this.helperEffectState = 'in';
+        this.helpedTarget = worstTarget;
       });
   }
 
