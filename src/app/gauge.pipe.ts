b3d525fdd000f91aba96f542e2d1f3fa5d4734ed
nakahararuu <repika02@gmail.com>
2017-12-11 06:18:41 +0900

    デザイン微修正

--- src/app/gauge.pipe.ts
+++ src/app/gauge.pipe.ts
@@ -6,15 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class GaugePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    let plusPointPart = value.plus / (value.plus + value.minus) * 100 || 0;
-    let minusPointPart = value.minus / (value.plus + value.minus) * 100 || 0;
+    let plusPointPart = Math.round(value.plus / (value.plus + value.minus) * 100) || 0;
+    let minusPointPart =  Math.round(value.minus / (value.plus + value.minus) * 100) || 0;
     if (plusPointPart === minusPointPart) {
       plusPointPart = 50;
       minusPointPart = 50;
     }
     return [
-      {value: plusPointPart, label: 'Testing'},
-      {value: minusPointPart, label: 'SpeedUp'}
+      {value: plusPointPart, type: 'info', label: `${plusPointPart}%`},
+      {value: minusPointPart, type: 'success', label: `${minusPointPart}%`}
     ];
   }
 
