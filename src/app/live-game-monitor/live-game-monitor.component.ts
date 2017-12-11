1fc3b84e438d6e939523cb297640c725a88d7449
nakahararuu <repika02@gmail.com>
2017-12-11 13:02:59 +0900

    アニメーションの描画時間調整

--- src/app/live-game-monitor/live-game-monitor.component.ts
+++ src/app/live-game-monitor/live-game-monitor.component.ts
@@ -12,10 +12,10 @@ import {trigger, state, style, transition, animate, keyframes} from '@angular/an
         transform: 'translateX(-200vw)'
       })),
       transition('void => in', [
-        animate(4000, keyframes([
+        animate(10000, keyframes([
           style({transform: 'translateX(100vw)', offset: 0}),
-          style({transform: 'translateX(calc(100vw - 1150px))', offset: 0.1}),
-          style({transform: 'translateX(calc(100vw - 1250px))', offset: 0.9}),
+          style({transform: 'translateX(calc(100vw - 1150px))', offset: 0.05}),
+          style({transform: 'translateX(calc(100vw - 1250px))', offset: 0.95}),
           style({transform: 'translateX(-200vw)', offset: 1})
         ]))
       ])
@@ -25,10 +25,10 @@ import {trigger, state, style, transition, animate, keyframes} from '@angular/an
         transform: 'scale(0)'
       })),
       transition('void => in', [
-        animate(4000, keyframes([
+        animate(10000, keyframes([
           style({transform: 'scale(0)', offset: 0}),
-          style({transform: 'scale(1)', offset: 0.1}),
-          style({transform: 'scale(1)', offset: 0.9}),
+          style({transform: 'scale(1)', offset: 0.05}),
+          style({transform: 'scale(1)', offset: 0.95}),
           style({transform: 'scale(0)', offset: 1})
         ]))
       ])
@@ -37,7 +37,7 @@ import {trigger, state, style, transition, animate, keyframes} from '@angular/an
 })
 export class LiveGameMonitorComponent implements OnInit {
 
-  private helperEffectState:  'in';
+  private helperEffectState: 'in';
   private helpedTarget: any;
 
   constructor(private route: ActivatedRoute,
